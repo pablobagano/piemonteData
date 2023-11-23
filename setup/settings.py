a9f71afee37b89435091dbab2ca29c062d6ca0ef
Pablo Diego Bagano Vilas Boas <pablobagano@Pablos-MacBook-Air-7.local>
2023-11-23 10:08:26 -0300

    App creation

--- setup/settings.py
+++ setup/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'piemonteData.apps.PiemontedataConfig'
 ]
 
 MIDDLEWARE = [
