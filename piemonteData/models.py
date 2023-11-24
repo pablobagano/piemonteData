002040604ae458477e06c69d51b2446e2e0a2258
Pablo Diego Bagano Vilas Boas <pablobagano@Pablos-MacBook-Air-7.local>
2023-11-24 19:34:13 -0300

    Gerencia and Diretoria

--- piemonteData/models.py
+++ piemonteData/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.tokens import default_token_generator
 from django.urls import reverse
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
-from django.contrib.sites.shortcuts import get_current_site
-from django.http import HttpRequest
+import logging
+import os
 # Create your models here.
 
 
@@ -16,29 +16,100 @@ class Diretoria(models.Model):
     sobrenome = models.CharField(max_length=20, null=False, blank=False)
     matricula = models.CharField(max_length=30, null=False, blank=False)
     email = models.EmailField()
+    email_sent = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.nome} {self.sobrenome}"
     
     def save(self, *args, **kwargs):
         super(Diretoria, self).save(*args, **kwargs)
-        username = str(self.nome).lower()+'.'+str(self.sobrenome).lower()
+        base_username = str(self.nome).lower()+'.'+str(self.sobrenome).lower()
+        username = base_username
+        num = 1
+        while User.objects.filter(username = username).exists():
+            username = f"{base_username}{num}"
+            num +=1 
         user, created  = User.objects.get_or_create(username=username)
         if created:
             user.first_name = self.nome
             user.last_name = self.sobrenome
-            user.is_superuser = True
+            user.email = self.email
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
             password_reset_url = reverse('password_reset_confirm', kwargs={'uidb64':uid, 'token':token})
-            send_mail(
+            try:
+                send_mail(
                 'Defina a sua senha',
                 f"Por favor defina a sua senha no link a seguir: {password_reset_url}",
                 [user.email], 
                 fail_silently = False
             )
+                self.email_sent = True
+            except Exception as e:
+                logger = logging.getLogger(__name__)
+                logger.error(f"Não foi possivel enviar email de confirmação: {e}")
+                self.email_sent = False
+                try:
+                    send_mail(
+                    'Inconsistência de cadastro',
+                    f"Não foi possível cadastra a senha de {user.username}",
+                    os.getenv('EMAIL_HOST_USER'),
+                    [os.getenv('EMAIL_HOST_USER')]
+                )
+                except Exception as fallback_error:
+                    logger.error(f"Erro ao enviar email de fallback: {fallback_error}")
+            
+            super(Diretoria, self).save(*args, **kwargs)
 
+class Gerencia(models.Model):
+    diretor = models.ForeignKey(Diretoria)
+    nome = models.CharField(max_length=20, null=False, blank=False)
+    sobrenome = models.CharField(max_length=30, null= False, blank=False)
+    email = models.EmailField()
+    email_sent = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
+    
+    def save(self, *args, **kwargs):
+        super(Gerencia, self).save()
+        base_username = str(self.nome).lower()+'.'+str(self.sobrenome).lower()
+        username = base_username
+        num = 1
+        while User.objects.filter(username= username).exists():
+            username = f"{base_username}{num}"
+            num +=1
+        user, created = User.objects.get_or_create(username=username)
+        if created:
+            user.first_name = self.nome
+            user.last_name = self.sobrenome
+            user.email = self.email
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            password_reset_url = reverse('password_reset_confirm', kwargs={'uidb64':uid, 'token':token})
+            try:
+                send_mail(
+                'Defina a sua senha',
+                f"Por favor defina a sua senha no link a seguir: {password_reset_url}",
+                [user.email], 
+                fail_silently = False
+            )
+                self.email_sent = True
+            except Exception as e:
+                logger = logging.getLogger(__name__)
+                logger.error(f"Não foi possivel enviar email de confirmação: {e}")
+                self.email_sent = False
+                try:
+                    send_mail(
+                    'Inconsistência de cadastro',
+                    f"Não foi possível cadastra a senha de {user.username}",
+                    os.getenv('EMAIL_HOST_USER'),
+                    [os.getenv('EMAIL_HOST_USER')]
+                )
+                except Exception as fallback_error:
+                    logger.error(f"Erro ao enviar email de fallback: {fallback_error}")
+                
+            super(Gerencia, self).save(*args, **kwargs)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
