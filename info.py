e64690530812f15c7821154af9f18dd5771f15c1
Pablo Diego Bagano Vilas Boas <pablobagano@Pablos-MacBook-Air-7.local>
2023-11-25 09:18:19 -0300

    Supervisao

---
+++ info.py
@@ -0,0 +1,4 @@
+bahia = 'Camaçari,Pindobaçu,Utinga,Seabra,Serrolândia,Iuiu,Salvador,Ibitiara,Saúde,Iaçu,VárzeaNova,Candeal,Capim Grosso,Ibotirama,A. Dourada,Sobradinho,Jacobina'.split(",")
+bahia = sorted(bahia)
+sergipe = 'Carira,Aracaju,São Cristóvão,N. Sª Da Glória,Neópolis,SimãoDias,Frei Paulo,Campo do Brito,Macambira'.split(",")
+sergipe = sorted(sergipe) 
--- piemonteData/models.py
+++ piemonteData/models.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from django.db import models
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
@@ -7,6 +8,7 @@ from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 import logging
 import os
+from info import * 
 # Create your models here.
 
 
@@ -62,7 +64,7 @@ class Diretoria(models.Model):
             super(Diretoria, self).save(*args, **kwargs)
 
 class Gerencia(models.Model):
-    diretor = models.ForeignKey(Diretoria)
+    diretor = models.ForeignKey(Diretoria, on_delete= models.CASCADE)
     nome = models.CharField(max_length=20, null=False, blank=False)
     sobrenome = models.CharField(max_length=30, null= False, blank=False)
     email = models.EmailField()
@@ -72,7 +74,7 @@ class Gerencia(models.Model):
         return f"{self.nome} {self.sobrenome}"
     
     def save(self, *args, **kwargs):
-        super(Gerencia, self).save()
+        super(Gerencia, self).save(*args, **kwargs)
         base_username = str(self.nome).lower()+'.'+str(self.sobrenome).lower()
         username = base_username
         num = 1
@@ -110,6 +112,57 @@ class Gerencia(models.Model):
                     logger.error(f"Erro ao enviar email de fallback: {fallback_error}")
                 
             super(Gerencia, self).save(*args, **kwargs)
+
+
+class Supervisao(models.Model):
+    diretoria = models.ForeignKey(Diretoria, on_delete= models.CASCADE)
+    gerencia = models.ForeignKey(Gerencia, on_delete= models.CASCADE)
+    nome = models.CharField(max_length=20, null=True, blank=False)
+    sobrenome = models.CharField(max_length=30, null=True, blank=False)
+    email = models.EmailField()
+    email_sent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
+    
+    def save(self, *args, **kwargs):
+        super(Supervisao, self).save(*args, **kwargs)
+        base_username = str(self.nome) + '.' + str(self.sobrenome)
+        username = base_username
+        num = 1
+        while User.objects.filter(username = username).exists():
+            username = f"{base_username}{num}"
+            num += 1
+        user, created = User.objects.get_or_create(username=username)
+        if created:
+            user.first_name = self.nome
+            user.last_name = self.sobrenome
+            user.email = self.email
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            password_reset_url = reverse('password_reset_confirm', kwargs={'uidb64':uid, 'token': token})
+            try:
+                send_mail(
+                    'Defina sua senha',
+                    f'Por favor defina sua senha no link a seguir: {password_reset_url}',
+                    os.getenv('EMAIL_HOST_USER'),
+                    [user.email]
+
+                )
+                self.email_sent = True
+            except Exception as e:
+                logger = logging.getLogger(__name__)
+                logger.error(f"Não foi possivel enviar email de confirmação: {e}")
+                self.email_sent = False
+                try:
+                    send_mail(
+                        'Inconsistência de cadastro',
+                        f"Não foi possível cadastrar a senha de {user.username}",
+                        os.getenv('EMAIL_HOST_USER'),
+                        [os.getenv('EMAIL_HOST_USER')]
+                    )
+                except Exception as fallback_error:
+                    logger.error(f"Erro ao enviar email de fallback: {fallback_error}")
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
