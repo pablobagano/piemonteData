c759afc2a6a6d0be1a73acb8028bdd58acbc50de
Pablo Diego Bagano Vilas Boas <pablobagano@Pablos-MacBook-Air-7.local>
2023-11-29 18:00:53 -0300

    Admin

--- piemonteData/admin.py
+++ piemonteData/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Diretoria, Gerencia, Supervisao, Agente
 
 # Register your models here.
+
+class Diretor(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'matricula', 'email')
+
+admin.site.register(Diretoria, Diretor)
+
+class Gerente(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'matricula', 'email', 'diretor')
+
+admin.site.register(Gerencia, Gerente)
+
+class Supervisor(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'matricula', 'email', 'diretor', 'gerente')
+
+admin.site.register(Supervisao, Supervisor)
+
+class Agente(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'matricula', 'email', 'diretor', 'gerente', 'supervisor')
+
+admin.site.register(Agente, Agente)
+
